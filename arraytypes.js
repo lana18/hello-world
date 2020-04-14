08d1576db15d4fe749283e5900b85f95255a5cc2
lana18 <36461418+lana18@users.noreply.github.com>
2020-04-15 11:30:11 +1200

    arraytypes.js
    basic array types: 
    .push, .pop, array concatenation, mutable, length, index.

---
+++ arraytypes.js
@@ -0,0 +1,41 @@
+//FIRST ARRAY- length
+let chores = ['dishes', 'rubbish', 'clean','bathroom']
+console.log(chores.length)
+
+
+//SECOND ARRAY-push
+const basket = ['shorts', 'top','hat']
+basket.push['shoes'];
+console.log(basket); 
+
+//THIRD ARRAY-pop
+const shoppingList = ['potatoes', 'kumara','toiletpaper']
+const newShoppingList= shoppingList.pop();
+console.log(shoppingList);
+
+
+//FOURTH ARRAY-mutable 
+const names = ['alex', 'mary', 'janet', 'rebecca'];
+names.push('tony');
+console.log(names);
+
+
+//FIFTH ARRAY-array
+const addLine = [1,2,3,4,5 ];
+
+const mixedLine = [ 'one', 2, false];
+
+newLine = [addLine + mixedLine];
+console.log(newLine);
+
+
+
+//SIXTH ARRAY-index 
+const firstArray = [20,30,40,50];
+
+console.log(firstArray[0]);
+console.log(firstArray[1]);
+console.log(firstArray[2]);
+console.log(firstArray[3]);
+
+
