51732b087de919ec851ce8ce5169b331e528692b
lana18 <36461418+lana18@users.noreply.github.com>
2020-04-16 16:37:20 +1200

    Update whaletalkdraft.js

--- whaletalkdraft.js
+++ whaletalkdraft.js
@@ -1,1 +1,27 @@
 
+let input = 'turpentine and turtles'
+const vowels=['a','e', 'i', 'o','u']
+
+let resultArray=[];
+
+for(let inputIndex=0; inputIndex <input.length; inputIndex++); {
+  //console.log('inputIndex= '' + inputIndex);
+}
+
+if 
+
+(input[inputIndex]===vowels[vowel]){
+if input[inputIndex]==='e' {
+  resultArray.push('ee');
+}
+else if(input[inputIndex]==='u'){
+  resultArray.push('u');
+}
+else{
+  resultArray.push(input[inputIndex]);
+
+}
+}
+
+console.log(resultArray.join(''). toUppperCase());
+
