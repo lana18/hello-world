42b4e7e0e1e858fc19e096d6502f1b86167b0ac8
lana18 <36461418+lana18@users.noreply.github.com>
2020-04-21 10:28:58 +1200

    Update object_ METHOD.js

--- object_ METHOD.js
+++ object_ METHOD.js
@@ -1,3 +1,5 @@
+//CAN ADD A METHOD onto AN OBJECT. Print an OBJECT by using the OBJECT NAME and DOT. 
+
 let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
 
 let alienShip = {
